5e8f4084548476209b354f807e7395e53d534fad
Lok Leoi <lyulelok@gmail.com>
2022-11-04 21:31:01 -0400

    lab7

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// @author: Le Lyu
+// @credit: Peiyu Zhong
 const margin = { top: 20, right: 30, bottom: 30, left: 40 };
 
 const width = 800,
@@ -10,6 +12,7 @@ const svg = d3
 
 d3.json("airports.json", d3.autoType).then((airports) => {
   d3.json("world-110m.json", d3.autoType).then((map) => {
+    let visType = d3.select("input[name=type]:checked").node().value;
     // creating the force layout
     // create scale for the circle sizing
     const size = d3
@@ -19,7 +22,47 @@ d3.json("airports.json", d3.autoType).then((airports) => {
     airports.nodes.forEach((d) => {
       d.r = size(d.passengers);
     });
+    // creating map before node in order for nodes to be on top of map
+    // conver topojson to geojson, extract state info
+    const features = topojson.feature(map, map.objects.countries).features;
 
+    const projection = d3.geoMercator().fitExtent(
+      [
+        [0, 0],
+        [width, height],
+      ],
+      topojson.feature(map, map.objects.countries)
+    );
+
+    let path = d3.geoPath().projection(projection);
+
+    const worldmap = svg
+      .selectAll("path")
+      .data(features)
+      .join("path")
+      .attr("class", "map");
+
+    d3.selectAll("input[name=type]").on("change", (event) => {
+      visType = event.target.value; // selected button
+      switchLayout();
+    });
+
+    worldmap
+      .append("path")
+      .attr("name", function (d) {
+        return d.properties.name;
+      })
+      .attr("id", function (d) {
+        return d.id;
+      });
+
+    svg
+      .append("path")
+      .datum(topojson.mesh(map, map.objects.countries))
+      .attr("d", path)
+      .attr("fill", "none")
+      .attr("stroke", "white")
+      .attr("class", "subunit-boundary");
     // creating force
     let force = d3
       .forceSimulation(airports.nodes)
@@ -57,99 +100,54 @@ d3.json("airports.json", d3.autoType).then((airports) => {
     }
 
     node.append("title").text((d) => d.name);
-    function drag(simulation) {
-      function dragstarted(event) {
-        if (!event.active) simulation.alphaTarget(0.3).restart();
-        event.subject.fx = event.subject.x;
-        event.subject.fy = event.subject.y;
-      }
 
-      function dragged(event) {
+    const drag = d3
+      .drag()
+      .filter((event) => visType === "force")
+      .on("start", (event) => {
+        if (!event.active) force.alphaTarget(0.3).restart();
         event.subject.fx = event.x;
         event.subject.fy = event.y;
-      }
-
-      function dragended(event) {
-        if (!event.active) simulation.alphaTarget(0);
+      })
+      .on("drag", (event) => {
+        event.subject.fx = event.x;
+        event.subject.fy = event.y;
+      })
+      .on("end", (event) => {
+        if (!event.active) force.alphaTarget(0);
         event.subject.fx = null;
         event.subject.fy = null;
-      }
-
-      return d3
-        .drag()
-        .on("start", dragstarted)
-        .on("drag", dragged)
-        .on("end", dragended)
-        .filter((event) => visType === "force");
-    }
-    // Set default visType to force
-    let visType = "force";
-    node.call(drag(force));
-
-    // creating map
-    // conver topojson to geojson, extract state info
-    const features = topojson.feature(map, map.objects.countries).features;
-
-    const projection = d3.geoMercator().fitExtent(
-      [
-        [0, 0],
-        [width, height],
-      ],
-      topojson.feature(map, map.objects.countries)
-    );
-
-    let path = d3.geoPath().projection(projection);
-    let worldmap = svg.append("g").attr("class", "map");
-
-    
-    d3.selectAll("input[name=type]").on("change", (event) => {
-      visType = event.target.value; // selected button
-      switchLayout();
-    });
-    worldmap
-      .selectAll("path")
-      .data(features)
-      .enter()
-      .append("path")
-      .attr("name", function (d) {
-        return d.properties.name;
-      })
-      .attr("id", function (d) {
-        return d.id;
-      })
-      .attr("d", path)
-      .attr("stroke", "white")
-      .attr("class", "subunit-boundary");
-    worldmap.attr("opacity", 0);
+      });
+    node.call(drag);
 
     function switchLayout() {
       if (visType === "map") {
-        // create the map
-
-        // stop the simulation
         force.stop();
-        // set the positions of links and nodes based on geo-coordinates
-        airports.nodes.forEach((d) => {
-          d.x = projection([d.longitude, d.latitude])[0]; // update x and use it for cx
-          d.y = projection([d.longitude, d.latitude])[1]; // update x and use it for cy
+        worldmap.attr("d", path);
 
-          node.attr("cx", (d) => d.x);
-          node.attr("cy", (d) => d.y);
-          link
-            .attr("x1", (d) => d.source.x)
-            .attr("y1", (d) => d.source.y)
-            .attr("x2", (d) => d.target.x)
-            .attr("y2", (d) => d.target.y);
-        });
-        // set the map opacity to 1
-        worldmap.attr("opacity", 1);
+        node
+          .transition()
+          .duration(500)
+          .attr("cx", function (d) {
+            d.x = projection([d.longitude, d.latitude])[0];
+            return d.x;
+          })
+          .attr("cy", function (d) {
+            d.y = projection([d.longitude, d.latitude])[1];
+            return d.y;
+          });
+        worldmap.transition().duration(500).attr("opacity", 1);
+
+        link
+          .transition()
+          .duration(500)
+          .attr("x1", (d) => d.source.x)
+          .attr("y1", (d) => d.source.y)
+          .attr("x2", (d) => d.target.x)
+          .attr("y2", (d) => d.target.y);
       } else {
-        // force layout
-        // restart the simulation
-        // issue: some nodes can't be dragged.
-        force.restart();
-        // set the map opacity to 0
-        worldmap.attr("opacity", 0);
+        force.alpha(1.0).restart();
+        worldmap.transition().duration(500).attr("opacity", 0);
       }
     }
   });
